b3874053f0d6d827a26ff8d1092d374ed1ff3168
Nicholas Baron <njbaron@cpp.edu>
2019-02-09 00:24:12 -0800

    Optimized the output of the program

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "starter.hpp"
 
 #include <atomic>
-#include <chrono>
 #include <future>
 #include <iostream>		// cout
 #include <queue>
@@ -26,22 +25,29 @@ int main(int arg_count, const char** args){
 	thread starter(fill_prime_queue, 
 					ref(prime_queue), ref(settings), ref(running));
 	
-	long long largest_queue_size = 1;
+	// The lower this value, the more time this thread spends in output
+	constexpr unsigned short flush_amount = 25;
 	
+	long long largest_queue_size = 1;
+	unsigned short count = 0;
 	while(running || !prime_queue.empty()){
 		
 		// Wait until there is at least one thing in the queue
-		while(prime_queue.empty()){ /*this_thread::yield();*/ }
+		// Repeatedly tell the scheduler that we are waiting
+		while(prime_queue.empty()){ this_thread::yield(); }
 		
+		// get() for a future is non-const
 		auto& next = prime_queue.front();
 		
 		try{
 			const auto res = next.get();
 			
 			if(settings.only_primes && res.second){
-				cout << res.first << endl;
+				cout << res.first << '\n';
+				count++;
 			}else if(!settings.only_primes){
-				cout << res	<< endl;
+				cout << res	<< '\n';
+				count++;
 			}
 
 			prime_queue.pop();
@@ -49,13 +55,21 @@ int main(int arg_count, const char** args){
 			cerr << "[Main] Future error: " << fut_err.what() << endl;
 		}catch(const exception& e){
 			cerr << "[Main] Error: " << e.what() << endl;
+		}catch(...){
+			cerr << "[Main] Unknown exception caught!" << endl;
 		}
 		
 		if(settings.debug_mode && !settings.single_mode){
-			cout << prime_queue.size() << endl;
+			cout << "Queue Size : " << prime_queue.size() << endl;
 		}
 		
-		largest_queue_size = max<long long>(prime_queue.size(), largest_queue_size);
+		largest_queue_size = max<long long>(prime_queue.size(), 
+											largest_queue_size);
+		
+		if(count >= flush_amount){
+			count = 0;
+			cout << flush;
+		}
 	}
 	
 	starter.join();
